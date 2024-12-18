d67e09c853925ffc06726fca3e9f1fc90adeac8f
abiman-dot <abi216075@gmail.com>
2024-12-19 01:23:36 +0530

    Create cloudinaryLoader.js

---
+++ botf/src/cloudinaryLoader.js
@@ -0,0 +1,18 @@
+// cloudinaryLoader.js
+export const loadCloudinaryScript = () => {
+    return new Promise((resolve, reject) => {
+      if (document.getElementById("cloudinary-script")) {
+        // Cloudinary script already loaded
+        resolve(window.cloudinary);
+        return;
+      }
+  
+      const script = document.createElement("script");
+      script.id = "cloudinary-script";
+      script.src = "https://widget.cloudinary.com/v2.0/global/all.js";
+      script.onload = () => resolve(window.cloudinary);
+      script.onerror = () => reject("Failed to load Cloudinary script");
+      document.body.appendChild(script);
+    });
+  };
+  
