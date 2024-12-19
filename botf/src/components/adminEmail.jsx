26bd7c1c19b99b734a4bfe4301ed27d75cd04c5e
abiman-dot <abi216075@gmail.com>
2024-12-19 21:53:08 +0530

    Update adminEmail.jsx

--- botf/src/components/adminEmail.jsx
+++ botf/src/components/adminEmail.jsx
@@ -89,6 +89,12 @@ const AdminEmail = () => {
     >
       Dashboard
     </button>
+           <button
+      onClick={() => navigate("/admin-pub")}
+      className="bg-red-600 text-white px-4 py-2 rounded-lg shadow hover:bg-teal-600 transition"
+    >
+      Published
+    </button>
         </div>
       
       </div>
