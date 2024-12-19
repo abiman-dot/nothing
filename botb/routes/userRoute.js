777f51d1eb8fce5a68b08b9fa19884dbc7ca0c2d
abiman-dot <abi216075@gmail.com>
2024-12-19 22:39:43 +0530

    Update userRoute.js

--- botb/routes/userRoute.js
+++ botb/routes/userRoute.js
@@ -1,11 +1,10 @@
 import express from "express";
-import {allLikes, createUser, dislikes, getuser, getusers, likes,updateUserEmail} from "../controllers/userCntrl.js";
+import {allLikes, createUser, dislikes, getuser, getusers, interest, likes, removeInterest, updateAgent, updateUser} from "../controllers/userCntrl.js";
  
 const router = express.Router();
 
 
 
-router.put("/updateuser", updateUserEmail);
 
 router.post("/register", createUser );
 router.post("/likes/:id", likes);
@@ -13,6 +12,12 @@ router.delete("/dislikes/:id",dislikes);
 router.post("/allLikes", allLikes);
 router.get("/allusers", getusers);
 router.post("/get", getuser);
+router.put("/updateUser", updateUser);
+router.put("/updateAgent", updateAgent);
+router.post("/addInterest/:id", interest);
+router.delete("/removeInterest/:id", removeInterest);
+router.post("/rentedbyagent/:id", interest);
+
 
 
 
